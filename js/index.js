942a59f37295bc8bcdc5720cf4f7a14d219a57d6
YukiKayanumayou <yuki.kayanuma@gmail.com>
2019-10-02 19:13:34 +0900

    js追加

--- js/index.js
+++ js/index.js
@@ -9,3 +9,14 @@ $(function(){
   })
 })
 
+$(function(){
+  $('a[href^="#"]').click(function() {
+     var speed = 400;
+     var href= $(this).attr("href");
+     var target = $(href == "#" || href == "" ? 'html' : href);
+     var position = target.offset().top -70;
+     $('body,html').animate({scrollTop:position}, speed, 'swing');
+     return false;
+  })
+})
+
